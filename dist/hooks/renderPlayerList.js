a72823c43ff87fd03095e61867b21fedef1f1339
Anthony Ronda <anthonyronda@gmail.com>
2021-05-11 22:47:58 -0400

    fix: remove console log debugging

--- dist/hooks/renderPlayerList.js
+++ dist/hooks/renderPlayerList.js
@@ -1,5 +1,4 @@
 const onRenderPlayerList = (_playerList, html) => {
-    // console.warn("help")
     document.body.style.setProperty("--playerlist-height", `${html.height() + 22}px`);
 };
 export default onRenderPlayerList;
--- src/hooks/renderPlayerList.ts
+++ src/hooks/renderPlayerList.ts
@@ -1,5 +1,5 @@
 const onRenderPlayerList = (_playerList, html) => {
-    // console.warn("help")
+    
     document.body.style.setProperty("--playerlist-height", `${html.height()+22}px`);
 
 }
