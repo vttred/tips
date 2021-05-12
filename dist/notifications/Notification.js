47284c187ffe1dcca0756f46b606b8a5c1608f8d
Anthony Ronda <anthonyronda@gmail.com>
2021-05-12 17:07:46 -0400

    fix: tip fadeIn() now correctly fades in

--- dist/notifications/Notification.js
+++ dist/notifications/Notification.js
@@ -7,9 +7,9 @@ const registerNotifications = () => {
     if ($("#tips-notifications").length === 0) {
         $("body").append(`<div id="tips-notifications" class="app"></div>`);
     }
-    if ($("#tips-hints").length === 0) {
-        $("body").append(`<div id="tips-hints"></div>`);
-    }
+    // if ($("#tips-hints").length === 0) {
+    //   $("body").append(`<div id="tips-hints"></div>`);
+    // }
     window.vtta =
         window.vtta ||
             {
@@ -24,7 +24,7 @@ const registerNotifications = () => {
                         // prettier-ignore
                         //$("#tips-notifications").css("bottom", $("#players").height() + (2 * MARGIN));
                         let note = $("#tips-notifications").append(message);
-                        $(note).fadeIn(200);
+                        $(note).hide().fadeIn(200);
                         if (timeout)
                             setTimeout(() => {
                                 $(note).fadeOut(200, () => {
@@ -39,80 +39,85 @@ const registerNotifications = () => {
                             });
                         });
                     },
-                },
-                hint: {
-                    clear: () => {
-                        $("#tips-hints div").hide(200, () => {
-                            $("#tips-hints").empty();
-                        });
-                    },
-                    show: (message, options = {}) => {
-                        return new Promise((resolve) => {
-                            $("#tips-hints").css("width", options.width ? options.width : 300);
-                            // construct the note
-                            let note = $(`<div style="display: none"></div>`);
-                            $(note).append(message);
-                            $(note).append('<div class="buttons"></div>');
-                            $("#tips-hints").append(note);
-                            $(note).fadeIn(200);
-                            if (!options.align)
-                                options.align = options.element ? "RIGHT" : "CENTER";
-                            let anchor = {
-                                width: 0,
-                                height: 0,
-                                top: Math.round(window.innerHeight / 2),
-                                left: Math.round(window.innerWidth / 2),
-                            };
-                            if (options.element) {
-                                anchor = Object.assign({ width: $(options.element).width(), height: $(options.element).height() }, $(options.element).offset());
-                            }
-                            const noteInfo = Object.assign({ width: $("#tips-hints").width(), height: $("#tips-hints").height() }, $("#tips-hints").position());
-                            switch (options.align) {
-                                case "RIGHT":
-                                    $("#tips-hints").css("top", anchor.top);
-                                    $("#tips-hints").css("left", anchor.left + anchor.width + MARGIN);
-                                    break;
-                                case "LEFT":
-                                    $("#tips-hints").css("top", anchor.top);
-                                    $("#tips-hints").css("left", anchor.left - noteInfo.width - MARGIN);
-                                    break;
-                                case "TOP":
-                                    $("#tips-hints").css("top", anchor.top - noteInfo.height - MARGIN);
-                                    $("#tips-hints").css("left", anchor.left);
-                                    break;
-                                case "BOTTOM":
-                                    $("#tips-hints").css("top", anchor.top + anchor.height + MARGIN);
-                                    $("#tips-hints").css("left", anchor.left);
-                                    break;
-                                default:
-                                    // eslint-disable-next-line no-mixed-operators
-                                    $("#tips-hints").css("top", anchor.top - Math.round(noteInfo.height / 2));
-                                    // eslint-disable-next-line no-mixed-operators
-                                    $("#tips-hints").css("left", anchor.left - Math.round(noteInfo.width / 2));
-                            }
-                            if (options.buttons) {
-                                for (let name of options.buttons) {
-                                    let btn = $("<button>" + name + "</button>");
-                                    $("div.buttons", note).append(btn);
-                                    $(btn).on("click", () => {
-                                        $(note).fadeOut(100, () => {
-                                            $(note).remove();
-                                            resolve(name);
-                                        });
-                                    });
-                                }
-                            }
-                            if (options.hide) {
-                                $(options.hide.selector).on(options.hide.event, () => {
-                                    $(note).fadeOut(100, () => {
-                                        $(note).remove();
-                                        resolve(true);
-                                    });
-                                });
-                            }
-                        });
-                    },
-                },
+                }
+                // hint: {
+                //   clear: () => {
+                //     $("#tips-hints div").hide(200, () => {
+                //       $("#tips-hints").empty();
+                //     });
+                //   },
+                //   show: (message, options: NotificationOptions = {}) => {
+                //     return new Promise((resolve) => {
+                //       $("#tips-hints").css("width", options.width ? options.width : 300);
+                //       // construct the note
+                //       let note = $(`<div style="display: none"></div>`);
+                //       $(note).append(message);
+                //       $(note).append('<div class="buttons"></div>');
+                //       $("#tips-hints").append(note);
+                //       $(note).fadeIn(200);
+                //       if (!options.align) options.align = options.element ? "RIGHT" : "CENTER";
+                //       let anchor = {
+                //         width: 0,
+                //         height: 0,
+                //         top: Math.round(window.innerHeight / 2),
+                //         left: Math.round(window.innerWidth / 2),
+                //       };
+                //       if (options.element) {
+                //         anchor = Object.assign(
+                //           { width: $(options.element).width(), height: $(options.element).height() },
+                //           $(options.element).offset()
+                //         );
+                //       }
+                //       const noteInfo = Object.assign(
+                //         { width: $("#tips-hints").width(), height: $("#tips-hints").height() },
+                //         $("#tips-hints").position()
+                //       );
+                //       switch (options.align) {
+                //         case "RIGHT":
+                //           $("#tips-hints").css("top", anchor.top);
+                //           $("#tips-hints").css("left", anchor.left + anchor.width + MARGIN);
+                //           break;
+                //         case "LEFT":
+                //           $("#tips-hints").css("top", anchor.top);
+                //           $("#tips-hints").css("left", anchor.left - noteInfo.width - MARGIN);
+                //           break;
+                //         case "TOP":
+                //           $("#tips-hints").css("top", anchor.top - noteInfo.height - MARGIN);
+                //           $("#tips-hints").css("left", anchor.left);
+                //           break;
+                //         case "BOTTOM":
+                //           $("#tips-hints").css("top", anchor.top + anchor.height + MARGIN);
+                //           $("#tips-hints").css("left", anchor.left);
+                //           break;
+                //         default:
+                //           // eslint-disable-next-line no-mixed-operators
+                //           $("#tips-hints").css("top", anchor.top - Math.round(noteInfo.height / 2));
+                //           // eslint-disable-next-line no-mixed-operators
+                //           $("#tips-hints").css("left", anchor.left - Math.round(noteInfo.width / 2));
+                //       }
+                //       if (options.buttons) {
+                //         for (let name of options.buttons) {
+                //           let btn = $("<button>" + name + "</button>");
+                //           $("div.buttons", note).append(btn);
+                //           $(btn).on("click", () => {
+                //             $(note).fadeOut(100, () => {
+                //               $(note).remove();
+                //               resolve(name);
+                //             });
+                //           });
+                //         }
+                //       }
+                //       if (options.hide) {
+                //         $(options.hide.selector).on(options.hide.event, () => {
+                //           $(note).fadeOut(100, () => {
+                //             $(note).remove();
+                //             resolve(true);
+                //           });
+                //         });
+                //       }
+                //     });
+                //   },
+                // },
             };
 };
 export default registerNotifications;
--- src/notifications/Notification.ts
+++ src/notifications/Notification.ts
@@ -36,7 +36,7 @@ interface Hint {
 
 interface VTTA {
   notification: Notification;
-  hint: Hint;
+  // hint: Hint;
 }
 
 const registerNotifications = (): void => {
@@ -45,9 +45,9 @@ const registerNotifications = (): void => {
   if ($("#tips-notifications").length === 0) {
     $("body").append(`<div id="tips-notifications" class="app"></div>`);
   }
-  if ($("#tips-hints").length === 0) {
-    $("body").append(`<div id="tips-hints"></div>`);
-  }
+  // if ($("#tips-hints").length === 0) {
+  //   $("body").append(`<div id="tips-hints"></div>`);
+  // }
 
   window.vtta =
     <VTTA>window.vtta ||
@@ -64,7 +64,7 @@ const registerNotifications = (): void => {
           //$("#tips-notifications").css("bottom", $("#players").height() + (2 * MARGIN));
 
           let note = $("#tips-notifications").append(message);
-          $(note).fadeIn(200);
+          $(note).hide().fadeIn(200);
 
           if (timeout)
             setTimeout(() => {
@@ -81,92 +81,92 @@ const registerNotifications = (): void => {
             });
           });
         },
-      },
-      hint: {
-        clear: () => {
-          $("#tips-hints div").hide(200, () => {
-            $("#tips-hints").empty();
-          });
-        },
-        show: (message, options: NotificationOptions = {}) => {
-          return new Promise((resolve) => {
-            $("#tips-hints").css("width", options.width ? options.width : 300);
+      }
+      // hint: {
+      //   clear: () => {
+      //     $("#tips-hints div").hide(200, () => {
+      //       $("#tips-hints").empty();
+      //     });
+      //   },
+      //   show: (message, options: NotificationOptions = {}) => {
+      //     return new Promise((resolve) => {
+      //       $("#tips-hints").css("width", options.width ? options.width : 300);
 
-            // construct the note
-            let note = $(`<div style="display: none"></div>`);
-            $(note).append(message);
-            $(note).append('<div class="buttons"></div>');
-            $("#tips-hints").append(note);
-            $(note).fadeIn(200);
+      //       // construct the note
+      //       let note = $(`<div style="display: none"></div>`);
+      //       $(note).append(message);
+      //       $(note).append('<div class="buttons"></div>');
+      //       $("#tips-hints").append(note);
+      //       $(note).fadeIn(200);
 
-            if (!options.align) options.align = options.element ? "RIGHT" : "CENTER";
+      //       if (!options.align) options.align = options.element ? "RIGHT" : "CENTER";
 
-            let anchor = {
-              width: 0,
-              height: 0,
-              top: Math.round(window.innerHeight / 2),
-              left: Math.round(window.innerWidth / 2),
-            };
+      //       let anchor = {
+      //         width: 0,
+      //         height: 0,
+      //         top: Math.round(window.innerHeight / 2),
+      //         left: Math.round(window.innerWidth / 2),
+      //       };
 
-            if (options.element) {
-              anchor = Object.assign(
-                { width: $(options.element).width(), height: $(options.element).height() },
-                $(options.element).offset()
-              );
-            }
-            const noteInfo = Object.assign(
-              { width: $("#tips-hints").width(), height: $("#tips-hints").height() },
-              $("#tips-hints").position()
-            );
+      //       if (options.element) {
+      //         anchor = Object.assign(
+      //           { width: $(options.element).width(), height: $(options.element).height() },
+      //           $(options.element).offset()
+      //         );
+      //       }
+      //       const noteInfo = Object.assign(
+      //         { width: $("#tips-hints").width(), height: $("#tips-hints").height() },
+      //         $("#tips-hints").position()
+      //       );
 
-            switch (options.align) {
-              case "RIGHT":
-                $("#tips-hints").css("top", anchor.top);
-                $("#tips-hints").css("left", anchor.left + anchor.width + MARGIN);
-                break;
-              case "LEFT":
-                $("#tips-hints").css("top", anchor.top);
-                $("#tips-hints").css("left", anchor.left - noteInfo.width - MARGIN);
-                break;
-              case "TOP":
-                $("#tips-hints").css("top", anchor.top - noteInfo.height - MARGIN);
-                $("#tips-hints").css("left", anchor.left);
-                break;
-              case "BOTTOM":
-                $("#tips-hints").css("top", anchor.top + anchor.height + MARGIN);
-                $("#tips-hints").css("left", anchor.left);
-                break;
+      //       switch (options.align) {
+      //         case "RIGHT":
+      //           $("#tips-hints").css("top", anchor.top);
+      //           $("#tips-hints").css("left", anchor.left + anchor.width + MARGIN);
+      //           break;
+      //         case "LEFT":
+      //           $("#tips-hints").css("top", anchor.top);
+      //           $("#tips-hints").css("left", anchor.left - noteInfo.width - MARGIN);
+      //           break;
+      //         case "TOP":
+      //           $("#tips-hints").css("top", anchor.top - noteInfo.height - MARGIN);
+      //           $("#tips-hints").css("left", anchor.left);
+      //           break;
+      //         case "BOTTOM":
+      //           $("#tips-hints").css("top", anchor.top + anchor.height + MARGIN);
+      //           $("#tips-hints").css("left", anchor.left);
+      //           break;
 
-              default:
-                // eslint-disable-next-line no-mixed-operators
-                $("#tips-hints").css("top", anchor.top - Math.round(noteInfo.height / 2));
-                // eslint-disable-next-line no-mixed-operators
-                $("#tips-hints").css("left", anchor.left - Math.round(noteInfo.width / 2));
-            }
+      //         default:
+      //           // eslint-disable-next-line no-mixed-operators
+      //           $("#tips-hints").css("top", anchor.top - Math.round(noteInfo.height / 2));
+      //           // eslint-disable-next-line no-mixed-operators
+      //           $("#tips-hints").css("left", anchor.left - Math.round(noteInfo.width / 2));
+      //       }
 
-            if (options.buttons) {
-              for (let name of options.buttons) {
-                let btn = $("<button>" + name + "</button>");
-                $("div.buttons", note).append(btn);
-                $(btn).on("click", () => {
-                  $(note).fadeOut(100, () => {
-                    $(note).remove();
-                    resolve(name);
-                  });
-                });
-              }
-            }
-            if (options.hide) {
-              $(options.hide.selector).on(options.hide.event, () => {
-                $(note).fadeOut(100, () => {
-                  $(note).remove();
-                  resolve(true);
-                });
-              });
-            }
-          });
-        },
-      },
+      //       if (options.buttons) {
+      //         for (let name of options.buttons) {
+      //           let btn = $("<button>" + name + "</button>");
+      //           $("div.buttons", note).append(btn);
+      //           $(btn).on("click", () => {
+      //             $(note).fadeOut(100, () => {
+      //               $(note).remove();
+      //               resolve(name);
+      //             });
+      //           });
+      //         }
+      //       }
+      //       if (options.hide) {
+      //         $(options.hide.selector).on(options.hide.event, () => {
+      //           $(note).fadeOut(100, () => {
+      //             $(note).remove();
+      //             resolve(true);
+      //           });
+      //         });
+      //       }
+      //     });
+      //   },
+      // },
     };
 };
 
