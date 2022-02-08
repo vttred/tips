cd1b9941486f32d6fb240a77c34b5543e0e38b4f
Anthony Ronda <anthonyronda@gmail.com>
2022-02-08 00:56:32 -0500

    chore: prettier formatting

--- src/scripts/getGame.ts
+++ src/scripts/getGame.ts
@@ -1,11 +1,11 @@
 /**
- * @exports getGame 
-*/
+ * @exports getGame
+ */
 function getGame(): Game {
-    if (!(game instanceof Game)) {
-        throw new Error("Foundry Tips module | Couldn't register modules settings because game is not initialized!");
-    }
-    return game;
+  if (!(game instanceof Game)) {
+    throw new Error("Foundry Tips module | Couldn't register modules settings because game is not initialized!");
+  }
+  return game;
 }
 
 export { getGame };
